977fcf6fb1c2704c5e02de47807542a96516e6fd
hayekznizam <45220116+hayekznizam@users.noreply.github.com>
2021-10-17 16:30:19 -0300

    Aula 7 - Script fora html e funções

--- tiacademy2/aula7/script/script.js
+++ tiacademy2/aula7/script/script.js
@@ -11,7 +11,45 @@ function verificaMaiorNum(num1, num2) {
   }
 }
 
-verificaMaiorNum(1, 3);
-verificaMaiorNum(10, 3);
-verificaMaiorNum(1000, 1445453);
-verificaMaiorNum(10, 10);
+// verificaMaiorNum(1, 3);
+// verificaMaiorNum(10, 3);
+// verificaMaiorNum(1000, 1445453);
+// verificaMaiorNum(10, 10);
+
+// function teste() {
+//   let nome = 'Vinícius';
+//   return nome;
+// }
+//console.log(teste());
+
+//arrow function
+// let msg = (nome) => {
+//   alert('estou feliz da vida com JS');
+// };
+
+let msg = (nome) => {
+  return nome;
+};
+//console.log(msg('Vinícius'));
+//funções auto executaveis
+
+function teste() {
+  alert('Estou na funcion teste');
+  return nome;
+}
+//função auto executavel
+// (function () {
+//   alert('Esta função me chamou sozinha');
+// })();
+
+// (function (produto, preco) {
+//   alert('O produto é ' + produto + ' e o preco é R$ ' + preco);
+// })('Biscoito', 2.35);
+let valor1 = prompt('Digite o valor 1');
+let valor2 = prompt('Digite o valor 2');
+const soma = (valor1, valor2) => {
+  let resultado = parseFloat(valor1) + parseFloat(valor2);
+  console.log(resultado);
+};
+
+soma(valor1, valor2);
